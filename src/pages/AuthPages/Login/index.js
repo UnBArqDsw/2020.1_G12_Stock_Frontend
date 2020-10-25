900eb71b805e7c1c6c2695ad6976e93ff076790d
Gabriel Davi <gdavi.gd@gmail.com>
2020-10-25 18:16:32 -0300

    init login

---
+++ src/pages/AuthPages/Login/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+export default function LoginPage() {
+  const [document, setDocument] = useState('');
+  const [password, setPassword] = useState('');
+
+  return <h1>Login page</h1>;
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export default function LoginPage() {
+  return <h1>Home</h1>;
+}
