a92c81e44389ef2704f50dd331831b9120f5aa94
Gabriel Davi <gdavi.gd@gmail.com>
2020-10-26 23:18:01 -0300

    add mask to input and toggle view to pass

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -3,7 +3,10 @@ import api from './Api';
 class AuthService {
   async signIn(document, password) {
     try {
-      const response = await api.post('/collaborator/auth', { document, password });
+      const response = await api.post('/collaborator/auth', {
+        document: document.replace(/\D/g, ''),
+        password,
+      });
       return response;
     } catch (error) {
       console.log(error);
