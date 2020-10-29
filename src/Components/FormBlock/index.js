46f1203e59f12c54f536ee10e51f0730dd939df4
micaellagouveia <micaella2212@gmail.com>
2020-10-29 19:10:04 -0300

    Add register company page
    Co-authored-by: Pedro Igor <pedroigor.silva@outlook.com>

---
+++ src/Components/FormBlock/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+export default function FormBlock(props) {
+
+    return (
+        <div className="info-block">
+        <div className="input-area">
+          <label htmlFor="company_name">{props.label1}:</label>
+          <br />
+          <input type={props.typeInput1} />
+        </div>
+        <div className="input-area">
+          <label htmlFor="document">{props.label2}:</label>
+          <br />
+          <input type={props.typeInput2} />
+        </div>
+      </div>
+    )
+}
--- src/Pages/RegisterPages/RegisterCompany/index.js
+++ src/Pages/RegisterPages/RegisterCompany/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useContext } from 'react';
 import logo from '../../../assets/images/logo-horizontal.png';
 import RegisterService from '../../../Services/RegisterService';
+import FormBlock from '../../../Components/FormBlock'
 import './styles.css';
 import InputMask from 'react-input-mask';
 import TextField from 'react';
 
-export default function RegisterCompanyPage(){
+export default function RegisterCompanyPage() {
   const [document, setDocument] = useState('');
   const [email, setEmail] = useState('');
   const [branch, setBranch] = useState('');
@@ -13,11 +14,11 @@ export default function RegisterCompanyPage(){
   const [telephone, setTelephone] = useState('');
   const [collaborator_quantity, setCollaboratorQuantity] = useState('');
 
-  async function registerCompany(){
-    try{
+  async function registerCompany() {
+    try {
       await RegisterService.registerCompany(email, document, branch, company_name, telephone, collaborator_quantity);
 
-    }catch(error){
+    } catch (error) {
       console.log(error.response);
     }
   }
@@ -26,11 +27,31 @@ export default function RegisterCompanyPage(){
     e.preventDefault();
     registerCompany(email, document, branch, company_name, telephone, collaborator_quantity);
   };
+
+
+
   return (
     <div className="container">
       <div className="register-company-header">
-        
+        <img src={logo} alt="logo" />
+        <div>Portal do Gestor</div>
       </div>
+      <div className="register-company-form">
+        <h1>Comece agora mesmo</h1>
+        <p>Preencha os dados abaixo para realizar o cadastro da sua empresa.</p>
+        <form onSubmit={onSubmit} >
+          <FormBlock label1="Nome da Empresa" label2="CNPJ/CPF" typeInput1="text" typeInput2="text"/>
+          <FormBlock label1="Email corporativo" label2="Telefone" typeInput1="text" typeInput2="text"/>
+          <FormBlock label1="Ramo da Empresa" label2="Quantidade de Funcionários" typeInput1="text" typeInput2="text"/>
+          <div className="register-company-submit">
+            <span>Ao enviar o formulário, você concorda com nossos termos e condições, assim como nossas políticas de privacidade</span>
+            <button type="submit">Cadastrar Empresa</button>
+          </div>
+
+        </form>
+      </div>
+
     </div>
+
   );
 }
