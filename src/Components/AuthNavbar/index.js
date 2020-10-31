0b4071d8309d8491b0811cc482af8c01a96339a8
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-10-30 23:14:56 -0300

    Adds cover, step and requirements sections

--- src/Components/AuthNavbar/index.js
+++ src/Components/AuthNavbar/index.js
@@ -6,18 +6,18 @@ import './styles.css';
 export default function AuthNavBar() {
 
   return (
-    <div className="container">
-      <div className="login-header">
-        <a href="/home">
-          <img src={logo} alt="logo" />
-        </a>
-        <p>Cadastrar Estoque</p>
-        <div className="singIn-page">
-          <a href="/login">
-            <button type="button">Portal do Gestor</button>
+      <div className="container">
+        <div className="login-header">
+          <a href="/home">
+            <img src={logo} alt="logo" />
           </a>
+          <p class="signUp">Cadastrar Estoque</p>
+          <div className="singIn-page">
+            <a href="/login">
+              <button class="signIn-button" type="button">Portal do Gestor</button>
+            </a>
+          </div>
         </div>
-      </div>
     </div>
   );
 }
--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,9 +1,45 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import Step1 from '../../assets/icons/step1.svg';
+import Step2 from '../../assets/icons/step2.svg';
+import Step3 from '../../assets/icons/step3.svg';
 import './styles.css';
 
 export default function HomePage() {
   return (
-   <h1></h1>
+    <body>
+      <div className="cover">
+        <h1 className="cover-title">O controle em sua mão</h1>
+        <span className="cover-text">Com o Stock, você acompanha seu fluxo de estoque em tempo real.</span>
+      </div>
+      <div className="steps">
+        <h1 className="steps-title">Comece já com apenas 3 passos</h1>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <img src={Step1} alt="Step1" />
+            <div>
+                <span>1. Cadastre sua empresa</span>
+            </div>
+            </div>
+            <div class="col-sm">
+              <img src={Step2} alt="Step2" />
+            <div>
+                <span>2. Acesse sua conta e adicione estoque e colaboradores</span>
+            </div>
+            </div>
+            <div class="col-sm">
+              <img src={Step3} alt="Step3" />
+              <div>
+                <span>3. Agora só fazer seu gerenciamento</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="requirements">
+        <h1 className="requirements-title">Saiba como funciona</h1>
+      </div>
+    </body>
   );
 }
