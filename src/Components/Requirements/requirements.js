e91666a36c1d8890c5a1670af9470ce000c39dd3
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-11-02 15:15:03 -0300

    Adds stock text

--- src/Components/Requirements/requirements.js
+++ src/Components/Requirements/requirements.js
@@ -12,7 +12,7 @@ const LOCALES = {
       name: "Controle de Estoque",
       title: "Facilidade de Controle",
       text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
+        "Através do Stock, é possível fazer seu controle de estoque de forma simples e fácil. Com design intuitivo, criar, atualizar e remover produtos fica muito mais simples. Além disso, é possível listar produtos por categorias, apresentando de forma clara a lista dos produtos separados pelas categorias que desejar.",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
     },
