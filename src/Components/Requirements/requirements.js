c8cb357471f5bb07a469ab78731b1267b9aab07e
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-11-02 21:10:21 -0300

    Adds texts at requirements section

--- src/Components/Requirements/requirements.js
+++ src/Components/Requirements/requirements.js
@@ -13,8 +13,6 @@ const LOCALES = {
       title: "Facilidade de Controle",
       text:
         "Através do Stock, é possível fazer seu controle de estoque de forma simples e fácil. Com design intuitivo, criar, atualizar e remover produtos fica muito mais simples. Além disso, é possível listar produtos por categorias, apresentando de forma clara a lista dos produtos separados pelas categorias que desejar.",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
     },
     {
       id: "Sales",
@@ -22,9 +20,7 @@ const LOCALES = {
       name: "Controle de Vendas",
       title: "Controle de Vendas",
       text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
-      description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
+      "Além do controle de estoque, o Stock ajuda a controlar suas vendas através do registro de vendas por funcionários e produtos, dados de vendas por categorias, colaboradores, produtos e data. É possível importar planilhas com dados, além de exportar seus dados em formato de planilha sempre que quiser. ",
   },
     {
       id: "Analysis",
@@ -32,9 +28,7 @@ const LOCALES = {
       name: "Análises",
       title: "Análises",
       text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
+        "Através de gráficos, a análise do seu negócio se torna muito mais prática e intuitiva. Com o painel de controle é possível ver dados de fluxo de saída do seu negócio, relação de vendas por período de tempo e produtos. Agora ficou muito mais simples saber em que produtos e período apostar suas vendas.",
     },
     {
       id: "AccessLevel",
@@ -42,9 +36,7 @@ const LOCALES = {
       name: "Nível de Acesso",
       title: "Nível de Acesso",
       text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim purus risus, non consequat purus dictum vitae. Curabitur fermentum vulputate dapibus. Maecenas sollicitudin neque a lectus viverra, maximus sagittis orci sagittis. Morbi enim mi, interdum et sapien sed, sodales sollicitudin turpis. Donec mauris massa, pharetra sed sapien sed, congue imperdiet ante.",
+      "Adicione diversos colaboradores ao seu controle, para o auxílio nas vendas, limitando seus níveis de acesso e dados que os colaboradores podem visualizar. É possível, ainda, administrar os dados de vendas de seus colaboradores. Tenha o controle do seu negócio em sua mão.",
   },
   ],
 };
