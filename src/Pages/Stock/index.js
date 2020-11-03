b64dc6956687eac2da38600e208a5634aa988c44
Gabriel Davi <gdavi.gd@gmail.com>
2020-11-03 20:29:41 -0300

    Adjust sidebar onFocus text color

--- src/Pages/Stock/index.js
+++ src/Pages/Stock/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Stock() {
   return (
     <div className="container">
-      <h1>Stock</h1>
+      <div className="stock-content">
+        <h1>Stock</h1>
+      </div>
     </div>
   );
 }
