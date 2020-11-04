65aa869e6424b810dfac37ce8537eb6bd81e38f7
Pedro Igor <pedroigor.silva@outlook.com>
2020-11-03 21:41:27 -0300

    #188 Create stock page and add buttons.
    Co-authored-by: Gabriel Davi <gdavi.gd@gmail.com>

---
+++ src/Components/ProductCard/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import {Card, CardBody} from 'reactstrap'; 
+
+export default function ProductCard(){
+  return(
+    <Card>
+      <CardBody>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+       
+      </CardBody>
+    </Card>
+  )
+}
--- src/Pages/Stock/index.js
+++ src/Pages/Stock/index.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import './styles.css';
+import {FaFilter, FaSortAmountUp} from 'react-icons/all';
+import {Table} from 'reactstrap'; 
 
 export default function Stock() {
   return (
     <div className="container">
       <div className="stock-content">
-        <h1>Stock</h1>
+        <h1>Estoque</h1>
+        <div className="toolbar">
+          <div className="search-filter">
+            <input type="text" placeholder="Procurar produtos"/>
+              <FaFilter />
+              <FaSortAmountUp />
+          </div>
+          <div className="create-import">
+            <button className="secondary">Importar</button>
+            <button className="secondary">Novo Produto</button>
+          </div>
+        </div>
       </div>
+
+      <Table hover style={{backgroundColor: "#fff", marginTop: 20}}>
+      <thead>
+        <tr>
+          <th>Nome e descrição</th>
+          <th>Quantidade</th>
+          <th>Unidade</th>
+          <th>Lotes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>
+           <p>Chá Camomila</p> 
+           <p>Chá de Camomila LEÃO 10g com 10 Saquinhos</p>
+            </th>
+          <td>10</td>
+          <td>100g</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+
     </div>
   );
 }
