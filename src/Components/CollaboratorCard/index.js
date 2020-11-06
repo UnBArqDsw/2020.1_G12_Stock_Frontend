e77a3477730b0c7946c164834da8ddf61f3d1563
micaellagouveia <micaella2212@gmail.com>
2020-11-06 15:43:02 -0300

    Add result modal and refresh page
    Co-authored-by: Sofia <sofiapatrocinio@hotmail.com>

--- src/Components/CollaboratorCard/index.js
+++ src/Components/CollaboratorCard/index.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import CollaboratorService from '../../Services/CollaboratorService';
 import { FaEdit } from 'react-icons/all';
 import ConfirmationModal from '../ConfirmationModal';
+import ResultModal from '../ResultModal';
 import './styles.css';
 
 export default function CollaboratorCard(props) {
 
   const { activate, idCollaborator, photo, name, accessLevel } = props;
   const [confirmationModal, setConfirmationModal] = useState(false);
+  const [resultModal, setResultModal] = useState(false);
 
   const changeStatusCollaborator = async () => {
     try {
       const response = await CollaboratorService.changeStatusCollaborator(idCollaborator, !activate);
       setConfirmationModal(false);
+      setResultModal(true)
       return response;
     } catch (error) {
       console.log(error);
@@ -38,9 +41,15 @@ export default function CollaboratorCard(props) {
           "Ao desativar, você não perderá os dados do(a) usuário(a), porém ele(a) não terá mais acesso à plataforma."
         ) : (
             "Ao ativar, o(a) colaborador(a) voltará a ter acesso à plataforma."
-          )} 
-          response={changeStatusCollaborator}/>
-        
+          )}
+        response={changeStatusCollaborator} />
+      <ResultModal modalVisible={resultModal}
+      setModalVisible={setResultModal}
+      title={activate ? (
+        `Colaborador(a) ${name} desativado(a) com sucesso!`
+      ) : (
+          `Colaborador(a) ${name} ativado(a) com sucesso!`
+        )} />
       <div className="container">
         <div className="card-header">
           <img src={photo} />
---
+++ src/Components/ResultModal/index.js
@@ -0,0 +1,32 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+export default function ConfirmationModal(props) {
+  const {
+    title,
+    modalVisible,
+    setModalVisible,
+  } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Modal isOpen={modalVisible} toggle={() => {
+        setModalVisible(!modalVisible);
+      }}>
+        <ModalHeader isOpen={modalVisible} toggle={() => {
+          setModalVisible(false)
+        }}>{title}
+        </ModalHeader>
+        <ModalFooter>
+          <Button color="primary" onClick={() => window.location.reload()}>Ok</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
