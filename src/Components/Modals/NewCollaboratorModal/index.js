253ca7c3400218d03a735bd9eb6289e1964fe514
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-11-07 18:37:05 -0300

    Fixes card style
    Co-authored-by: Micaella <micaella2212@gmail.com>

--- src/Components/Modals/NewCollaboratorModal/index.js
+++ src/Components/Modals/NewCollaboratorModal/index.js
@@ -39,7 +39,9 @@ export default function NewCollaboratorModal(props) {
     if (password && passwordConfirm && password === passwordConfirm) {
       try {
         const response = await CollaboratorService.createCollaborator(user.idCompany, name, cpf, email, idAccessLevel, password);
-        setResponseCreate(response)
+        setResponseCreate(response);
+        setResultModal(true);
+        setModalVisible(false);
         return response;
       } catch (error) {
       }
@@ -63,77 +65,79 @@ export default function NewCollaboratorModal(props) {
   };
 
   return (
-    <Modal isOpen={modalVisible} toggle={() => {
-      setModalVisible(!modalVisible);
-    }}>
-      <ModalHeader isOpen={modalVisible} toggle={() => {
-        setModalVisible(false)
-      }}>Cadastrar novo(a) Colaborador(a)</ModalHeader>
+    <>
       <ResultModal title={responseCreate.error || !responseCreate ? ("Falha ao adicionar colaborador(a).") : ("Colaborador(a) adicionado com sucesso!")}
         modalVisible={resultModal}
         setModalVisible={setResultModal} />
-      <div className="new-product-container">
-        <form onSubmit={onSubmit}>
-          <ModalBody>
+      <Modal isOpen={modalVisible} toggle={() => {
+        setModalVisible(!modalVisible);
+      }}>
+        <ModalHeader isOpen={modalVisible} toggle={() => {
+          setModalVisible(false)
+        }}>Cadastrar novo(a) Colaborador(a)</ModalHeader>
+        <div className="new-product-container">
+          <form onSubmit={onSubmit}>
+            <ModalBody>
 
-            <div className="new-product-input-container">
-              <div>
-                <label htmlFor="name">Nome</label>
-                <input type="text" onChange={(e) => setName(e.target.value)} required/>
+              <div className="new-product-input-container">
+                <div>
+                  <label htmlFor="name">Nome</label>
+                  <input type="text" onChange={(e) => setName(e.target.value)} required />
+                </div>
+                <div>
+                  <label htmlFor="document">CPF</label>
+                  <InputMask
+                    onChange={(e) => setCpf(e.target.value)}
+                    id="document"
+                    mask="999.999.999-99"
+                    maskChar=""
+                    required />
+                </div>
               </div>
-              <div>
-                <label htmlFor="document">CPF</label>
-                <InputMask
-                  onChange={(e) => setCpf(e.target.value)}
-                  id="document"
-                  mask="999.999.999-99"
-                  maskChar=""
-                  required />
+              <div className="new-product-input-container">
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input type="text" onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div>
+                  <label htmlFor="idAccessLevel">Nível de Acesso</label>
+                  <select type="select" onChange={(e) => setIdAccessLevel(e.target.value)}>
+                    <option value=""></option>
+                    {roles?.map((role, i) => (
+                      <option key={i} value={role.idAccessLevel}>
+                        {role.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="new-product-input-container">
-              <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" onChange={(e) => setEmail(e.target.value)} required/>
+              <div className="new-product-input-container">
+                <div>
+                  <label htmlFor="password">Senha</label>
+                  <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                  <label htmlFor="passwordConfirm">Confirmar Senha</label>
+                  <input type="password" onChange={function (e) {
+                    setPasswordConfirm(e.target.value);
+                    checkPassword(e.target.value);
+                  }}
+                    required />
+                  <p className="password-text">{checkPasswordsText}</p>
+                </div>
               </div>
-              <div>
-                <label htmlFor="idAccessLevel">Nível de Acesso</label>
-                <select type="select" onChange={(e) => setIdAccessLevel(e.target.value)}>
-                  <option value=""></option>
-                  {roles?.map((role, i) => (
-                    <option key={i} value={role.idAccessLevel}>
-                      {role.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-            <div className="new-product-input-container">
-              <div>
-                <label htmlFor="password">Senha</label>
-                <input type="password" onChange={(e) => setPassword(e.target.value)} />
-              </div>
-              <div>
-                <label htmlFor="passwordConfirm">Confirmar Senha</label>
-                <input type="password" onChange={function (e) {
-                  setPasswordConfirm(e.target.value);
-                  checkPassword(e.target.value);
-                }}
-                  required />
-                <p className="password-text">{checkPasswordsText}</p>
-              </div>
-            </div>
 
-          </ModalBody>
-          <ModalFooter>
-            <div className="add-product-modal-footer">
-              <button type="submit" className="secondary" disabled={!password || !passwordConfirm || !idAccessLevel || !email || !cpf || !name} onClick={() => setResultModal(true)}>
-                Adicionar
+            </ModalBody>
+            <ModalFooter>
+              <div className="add-product-modal-footer">
+                <button type="submit" className="secondary" disabled={!password || !passwordConfirm || !idAccessLevel || !email || !cpf || !name}>
+                  Adicionar
           </button>
-            </div>
-          </ModalFooter>
-        </form>
-      </div>
-    </Modal>
+              </div>
+            </ModalFooter>
+          </form>
+        </div>
+      </Modal>
+    </>
   );
 }
