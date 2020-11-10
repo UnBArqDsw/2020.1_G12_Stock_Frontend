b7adc39093eaf5c99a8486362e0455b2dac557d6
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-11-10 17:39:12 -0300

    Adds filter field

--- src/Pages/History/index.js
+++ src/Pages/History/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FaFilter } from 'react-icons/all';
+import './styles.css'
 
 export default function History() {
   return (
     <div className="container">
-      <h1>History</h1>
+      <div className="history-content">
+        <h1>Histórico</h1>
+      </div>
+      <div className="history-input-container">
+        <span>Filtrar por:</span>
+        <div className="history-icon-container">
+          <FaFilter />
+        </div>
+        <input id="history" />
+      </div>
     </div>
   );
 }
