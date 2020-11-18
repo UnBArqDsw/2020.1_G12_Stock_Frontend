11ae200146c3c64b4e0cad2fb7f214c73966057f
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-11-18 18:22:49 -0300

    Adds outflow graphic component

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import OutflowGraphic from '../../Components/Graphics/OutflowGraphic';
 
 export default function Dashboard() {
   return (
     <div className="container">
       <h1>Dashboard</h1>
+      <OutflowGraphic />
     </div>
   );
 }
