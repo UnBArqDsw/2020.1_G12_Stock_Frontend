f291cb975dc630724d8a137d33abfb9c3dc606f3
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2020-11-19 18:50:19 -0300

    Fixes outflow graphic styles

--- src/Components/Graphics/OutflowGraphic/index.js
+++ src/Components/Graphics/OutflowGraphic/index.js
@@ -1,10 +1,10 @@
-import React, { PureComponent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
-import moment from 'moment';
 import 'moment/locale/pt-br';
 import HistoryService from '../../../Services/HistoryService'
+import './styles.css'
 
 
 export default function OutflowGraphic() {
@@ -27,21 +27,27 @@ export default function OutflowGraphic() {
     }
 
     return (
-        <LineChart
-            width={500}
-            height={300}
-            data={salesData}
-            margin={{
-                top: 5, right: 30, left: 20, bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="Vendas" stroke="#72ACB0" />
-        </LineChart>
+        <div className="container">
+            <div className="card-header">
+                <div className="card-content">
+                    <h3>Fluxo de Saída</h3>
+                    <LineChart
+                        width={1000}
+                        height={250}
+                        data={salesData}
+                        margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="Vendas" stroke="#72ACB0" />
+                    </LineChart>
+                </div>
+            </div>
+        </div>
     );
-
 }
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import OutflowGraphic from '../../Components/Graphics/OutflowGraphic';
+import './styles.css'
 
 export default function Dashboard() {
   return (
     <div className="container">
-      <h1>Painel de Controle</h1>
-      <OutflowGraphic />
+      <div className="dashboard-content">
+        <h1>Painel de Controle</h1>
+        <div className="row">
+          <div className="col-md-6">
+          </div>
+          <div className="col-md-6">
+          </div>
+        </div>
+        <div className="row">
+          <OutflowGraphic />
+        </div>
+      </div>
     </div>
   );
 }
